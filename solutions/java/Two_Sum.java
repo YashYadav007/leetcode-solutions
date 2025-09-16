5d50a73d1e44a05fe9ae2ed9634879dea3fb6b74
YashYadav007 <115279187+YashYadav007@users.noreply.github.com>
2025-09-17 03:30:07 +0530

    Update solution: Two Sum

--- solutions/java/Two_Sum.java
+++ solutions/java/Two_Sum.java
@@ -1,1 +1,10 @@
-class Solu
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i =0;
+        while(i<nums.length){
+                int j=i+1;
+                while(j<nums.length){
+                    if(nums[j]+nums[i]==target){
+                        return new int[] {i,j};
+                    }
+                    j++;
