81514228c76c06e9c9da0836eadfad9c1d59ec6f
YashYadav007 <115279187+YashYadav007@users.noreply.github.com>
2025-09-28 19:20:19 +0530

    Update solution: Reverse Linked List

--- solutions/java/Reverse_Linked_List.java
+++ solutions/java/Reverse_Linked_List.java
@@ -10,6 +10,21 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode prev = null;
+		ListNode next = head.next;
+		ListNode curr = head;
+		
+		while(curr.next!=null) {
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+			next = curr.next;
+		}
+		curr.next = prev;
+		head = curr;
+        return head;
     }
 }
